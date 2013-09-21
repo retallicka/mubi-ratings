7ecc2a9aa7cb18690c86c093e37eed13233225a2
Vickie Retallick <vretallick@gmail.com>
2013-09-21 13:10:45 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MubiRatings::Application.config.secret_token = '2076d8f2515bc657dd33710fb9912c56fb4e34bd7de1e17e4c6600d8e3b55ea6a0aad39ed34f4173cd2b41544b5f350e0a43be25baa418256062cd64d128448d'
