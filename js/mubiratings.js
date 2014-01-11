4ddca1ebfa90bf89813c78f82d32210e26ccadd6
Vickie Retallick <vretallick@gmail.com>
2014-01-11 19:10:10 +0000

    Made some checks to ensure the right films are returning, refactored a bit. All films on UK mubi site are currently returning the right films

--- js/mubiratings.js
+++ js/mubiratings.js
@@ -4,7 +4,6 @@
 
 var mubiRating = {
   makeAPICall: function (url, func) {
-    console.log("make call");
     $.getJSON(url, function (data) {
       func(data);
     });
@@ -18,13 +17,46 @@ var mubiRating = {
 
   apiAvailable: function (data) {
     mubiRating.moviesAPI = data.links.movies;
-    mubiRating.findFilmInPage();
+    mubiRating.findFilmInPage(true);
   },
 
-  findFilmInPage: function () {
+  findFilmInPage: function (useSecondary) {
+    var name = mubiRating.getNameFromPage(useSecondary);
+
+    if (name) {
+      mubiRating.getFilm(name);
+    }
+  },
+
+  getNameFromPage: function (useSecondary) {
+    var name;
     var holder = $('.film_slider').find('.film_showcase:visible');
-    var name = holder.find('.film_title')[1].outerText;
-    mubiRating.getFilm(name);
+    var prev_nav = $('#prev_film');
+    var next_nav = $('#next_film');
+
+    var index = mubiRating.findDisplayedFilmIndex(prev_nav, next_nav);
+
+    name = holder.find('.film_title')[index].outerText;
+    if (useSecondary == true) {
+      name += " " + holder.find('.film-title-secondary')[index].outerText;
+    }
+
+    mubiRating.setYear(holder, index);
+
+    return name;
+  },
+
+  setYear: function (holder, index) {
+    var year_and_country = holder.find('.film_country_year')[index].outerText;
+    var pattern = /[0-9]+/;
+    mubiRating.year = year_and_country.match(pattern);
+  },
+
+  findDisplayedFilmIndex: function (prev_nav, next_nav) {
+    if ((prev_nav.length > 0 && next_nav.length > 0) || next_nav.length > 0) {
+      return 1;
+    }
+    return 0;
   },
 
   getFilm: function (filmname) {
@@ -38,18 +70,39 @@ var mubiRating = {
   },
 
   filmFound: function (json) {
-    if (json.movies && json.movies[0]) {
-      var rating = json.movies[0].ratings.critics_score;
-      console.log("The top film is " + json.movies[0].title);
-      console.log("Its year is " + json.movies[0].year);
-      console.log("This film is rated " + rating);
+    if (json.movies.length === 0 && mubiRating.searchComplete === false) {
+      console.log("No movies returned");
+      mubiRating.searchAgainWithoutSecondary();
+      mubiRating.searchComplete = true;
+    }
 
-      if (rating === -1) {
-        console.log("this film is unrated");
+    var closeMatch;
+    var match;
+
+    for (i = 0; i < json.movies.length; i++) {
+      var rating = json.movies[i].ratings.critics_score;
+      console.log("Looking at " + json.movies[i].title);
+
+      if (mubiRating.year == json.movies[i].year) {
+        match = i;
+        console.log("The year matches :)");
+        break;
       } else {
-        mubiRating.setHTML(rating);
+        if (closeMatch == undefined) {
+          closeMatch = i;
+        }
+        console.log("Year mismatch! our movie's year is meant to be " + mubiRating.year + " but we found " + json.movies[i].year);
       }
     }
+    if (match != undefined) {
+      mubiRating.setRating(json.movies[match].ratings.critics_score);
+    } else if (closeMatch != undefined) {
+      mubiRating.setRating(json.movies[closeMatch].ratings.critics_score);
+    }
+  },
+
+  searchAgainWithoutSecondary: function () {
+    mubiRating.findFilmInPage(false);
   },
 
   // Mubi Overlay
@@ -57,6 +110,14 @@ var mubiRating = {
   injectCSS: function () {
     var url = chrome.extension.getURL('../css/styles.css');
     $("head").append("<link id='mubirating' href='" + url + "' type='text/css' rel='stylesheet' />");
+  },
+
+  setRating: function (rating) {
+    if (rating === -1) {
+      console.log("this film is unrated");
+    } else {
+      mubiRating.setHTML(rating);
+    }
   },
 
   setHTML: function (score) {
@@ -76,10 +137,10 @@ var mubiRating = {
 };
 
 $("#film_navigation").click(function () {
-  console.log("previous");
   mubiRating.clearPrevious();
   if (mubiRating.moviesAPI) {
-    mubiRating.findFilmInPage();
+    mubiRating.searchComplete = false;
+    mubiRating.findFilmInPage(true);
   }
 });
 
