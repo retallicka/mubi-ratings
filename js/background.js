83c2ef2b4994d2019bc9d55fec55f57764a66e82
Vickie Retallick <vretallick@gmail.com>
2015-01-17 16:02:42 +0100

    Fix for Chrome 38, HTTP requests made in background

---
+++ js/background.js
@@ -0,0 +1,34 @@
+/**
+ * Possible parameters for request:
+ *  action: "xhttp" for a cross-origin HTTP request
+ *  method: Default "GET"
+ *  url   : required, but not validated
+ *  data  : data to send in a POST request
+ *
+ * The callback function is called upon completion of the request */
+ console.log('starting bg');
+chrome.runtime.onMessage.addListener(function(request, sender, callback) {
+    console.log('123');
+    if (request.action == "xhttp") {
+        var xhttp = new XMLHttpRequest();
+        var method = request.method ? request.method.toUpperCase() : 'GET';
+
+        xhttp.onload = function() {
+            console.log('onload');
+            callback(xhttp.responseText);
+        };
+        xhttp.onerror = function() {
+            console.log('error');
+            // Do whatever you want on error. Don't forget to invoke the
+            // callback to clean up the communication port.
+            callback();
+        };
+        xhttp.open(method, request.url, true);
+        if (method == 'POST') {
+            console.log('post');
+            xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        }
+        xhttp.send(request.data);
+        return true; // prevents the callback from being called too early on return
+    }
+});
--- js/mubiratings.js
+++ js/mubiratings.js
@@ -1,11 +1,16 @@
-// Mubi Ratings Chrome Extension. Developed by Vickie Retallick 2013
+// Mubi Ratings Chrome Extension. Developed by Victoria Retallick 2013-2015
 
-console.log("starting")
+// console.log("starting")
 
 var mubiRating = {
   makeAPICall: function (url, func) {
-    $.getJSON(url, function (data) {
-      func(data);
+    chrome.runtime.sendMessage({
+      method: 'GET',
+      action: 'xhttp',
+      url: url
+    }, function(responseText) {
+      // console.log(responseText);
+        func($.parseJSON(responseText));
     });
   },
 
@@ -15,8 +20,12 @@ var mubiRating = {
       '&format=json', mubiRating.apiAvailable);
   },
 
+
+
+
   apiAvailable: function (data) {
-    mubiRating.moviesAPI = data.links.movies;
+    // console.log('data is',data);
+    mubiRating.moviesAPI = data["links"].movies;
     mubiRating.findFilmInPage(true);
   },
 
@@ -32,12 +41,12 @@ var mubiRating = {
     var name = $('h1.film-title')[0].outerText;
     
     if (name == undefined) return;
-    console.log("Name is",name)
-    console.log($('h2.film-alternative-title'));
+    // console.log("Name is",name)
+    // console.log($('h2.film-alternative-title'));
 
     if (useSecondary == true && $('h2.film-alternative-title').length > 0) {
       name += " " + $('h2.film-alternative-title')[0].outerText;
-      console.log("Alt name is",$('h2.film-alternative-title')[0].outerText);
+      // console.log("Alt name is",$('h2.film-alternative-title')[0].outerText);
     }
 
     mubiRating.setYear();
@@ -46,7 +55,7 @@ var mubiRating = {
 
   setYear: function () {
     mubiRating.year = year = $('span.year')[0].outerText;
-    console.log("Year is ",mubiRating.year)
+    // console.log("Year is ",mubiRating.year)
   },
 
   findDisplayedFilmIndex: function () {
@@ -60,7 +69,7 @@ var mubiRating = {
   },
 
   getFilm: function (filmname) {
-    console.log("OK...preparing to search for " + filmname);
+    // console.log("OK...preparing to search for " + filmname);
     mubiRating.makeAPICall(this.moviesAPI +
       '?apikey=qurnwj6rx3cg5ggb63mju2ku' +
       '&format=json' +
@@ -80,17 +89,17 @@ var mubiRating = {
 
     for (i = 0; i < json.movies.length; i++) {
       var rating = json.movies[i].ratings.critics_score;
-      console.log("Looking at " + json.movies[i].title);
+      // console.log("Looking at " + json.movies[i].title);
 
       if (mubiRating.year == json.movies[i].year) {
         match = i;
-        console.log("The year matches :)");
+        // console.log("The year matches :)");
         break;
       } else {
         if (closeMatch == undefined) {
           closeMatch = i;
         }
-        console.log("Year mismatch! our movie's year is meant to be " + mubiRating.year + " but we found " + json.movies[i].year);
+        // console.log("Year mismatch! our movie's year is meant to be " + mubiRating.year + " but we found " + json.movies[i].year);
       }
     }
     if (match != undefined) {
@@ -113,7 +122,7 @@ var mubiRating = {
     if (rating === -1) {
       console.log("this film is unrated");
     } else {
-      console.log("the rating will be",rating);
+      // console.log("the rating will be",rating);
       mubiRating.setHTML(rating, link);
     }
   },
@@ -135,7 +144,6 @@ var mubiRating = {
   },
 
   clickNavigation: function () {
-    console.log("click nav");
     mubiRating.clearPrevious();
     if (mubiRating.moviesAPI) {
       mubiRating.searchComplete = false;
@@ -148,12 +156,8 @@ $("#film_navigation").click(function () {
   mubiRating.clickNavigation();
 });
 
-// $("a.film_still_link").click(function () {
-//   setTimeout(mubiRating.clickNavigation(), 1000);
-// });
-
 $(document).ready(function () {
-  console.log("ready");
+  // console.log("ready");
   mubiRating.searchComplete = false;
   mubiRating.requestAPI();
 });
